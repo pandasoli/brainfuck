26b6b5086e5677a95ac360cbab5d0e4333980e49
Eliton-Lisboa <elitonlisboa303@gmail.com>
2021-04-12 17:36:16 -0300

    add

--- brainfuck-interpreter.js
+++ brainfuck-interpreter.js
@@ -1,33 +1,35 @@
 
-function * bf(_code = '', _getChar = () => prompt('enter a char:')) {
-	const memory = '000000000000000000000000000'.split('').map(item => Number(item))
+function * bf(_code, _pause, _getChar) {
+	const code = String(_code || '').split('')
+	const getChar = _getChar || ( () => prompt('enter a char:') )
+	const memory = [0]
 	const acceptedChars = '+-<>.,[]!'
 	let pointer = 0
 	let result = ''
-	const loop = { open: 0, start: [] }
+	const loop = []
 
-	String(_code).split('')
-	for (let x = 0; x < _code.length; x++) {
+	for (let x = 0; x < code.length; x++) {
 		do {
-			const char = _code[x]
+			const char = code[x]
 
-			if (acceptedChars.includes(char)) { yield { mem: memory, poin: pointer, res: result, char: x, ascii: char } }
+			if (acceptedChars.includes(char)) {
+				if (_pause) { yield { memory, pointer, result, index: x, char } }
 
-			if (char == '+') { memory[pointer]++; if (memory[pointer] == 256) { memory[pointer] = 0 } }
-			if (char == '-') { memory[pointer]--; if (memory[pointer] == -1) { memory[pointer] = 255 } }
-			if (char == '>') { if (pointer < memory.length - 1) { pointer++ } }
-			if (char == '<') { if (pointer > 0) { pointer-- } }
-			if (char == '.') { result += String.fromCharCode( memory[pointer] ) }
-			if (char == ',') { memory[pointer] = _getChar().charCodeAt(0) }
-			if (char == '!') { return { mem: memory, poin: pointer, res: result, char: x, ascii: char } }
-			if (char == '[') { loop.start.unshift(x) }
-			if (char == ']') { if (memory[pointer] == 0) { loop.start.shift() } else { x = loop.start[0] } }
+				if (char === '+') { memory[pointer] = (memory[pointer] === 255) ? 0 : memory[pointer] + 1 }
+				else if (char === '-') { memory[pointer] = (memory[pointer] === 0) ? 255 : memory[pointer] - 1 }
+				else if (char === '>') { pointer++; (pointer === memory.length) ? memory.push(0) : '' }
+				else if (char === '<') { pointer--; if (pointer === -1) { throw 'Error: pointer can not be less than 1' } }
+				else if (char === '.') { result += String.fromCharCode( memory[pointer] ) }
+				else if (char === ',') { memory[pointer] = getChar().charCodeAt(0) }
+				else if (char === '!') { break }
+				else if (char === '[') { loop.unshift(x) }
+				else if (char === ']') { (memory[pointer] === 0) ? loop.shift() : x = loop[0] }
 
-			if (acceptedChars.includes(char)) { yield { mem: memory, poin: pointer, res: result, char: x, ascii: char } }
+				if (_pause) { yield { memory, pointer, result, index: x, char } }
+			}
 
-			if (loop.open > 0) { x++ }
-		} while (loop.open > 0)
+		} while (loop.length > 0 && x++)
 	}
 
-	return { mem: memory, poin: pointer, res: result, char: x, ascii: char }
+	return { memory, pointer, result }
 }
