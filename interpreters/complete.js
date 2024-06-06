608c357cb747661e5fce93f1a3f2f037e4629e9b
Panda Soli <pandasoli+git@vivaldi.net>
2024-06-06 02:35:44 -0300

    fix comment, export it, use ASCII (127, not 255)

--- interpreters/complete.js
+++ interpreters/complete.js
@@ -13,7 +13,7 @@
  * @property {number} ip - Intruction pointer
  * @property {string} res - The outputed result
  * @property {function(): string} prompt - Input function
- * @property {boolean} - Pause when found valid character
+ * @property {boolean} pause - Pause when found valid character
  */
 
 /**
@@ -22,7 +22,7 @@
 * @yields {BfResult}
 * @returns {BfResult}
 */
-function * bfinterpreter(code = '', config = {}) {
+export function * bfinterpreter(code = '', config = {}) {
   const mem = config.mem ?? [0]
   let ptr = config.ptr ?? 0
   let res = config.res ?? ''
@@ -49,10 +49,10 @@ function * bfinterpreter(code = '', config = {}) {
 		const ch = code[ip]
 		if (config.pause && '+-><.,![]'.includes(ch)) yield { mem, ptr, ip, res }
 
-		if      (ch == '+') mem[ptr] = mem[ptr] == 255 ? 0 : mem[ptr] + 1
-		else if (ch == '-') mem[ptr] = mem[ptr] == 0 ? 255 : mem[ptr] - 1
-		else if (ch == '>') {ptr++; ptr >= mem.length && mem.push(0)}
-		else if (ch == '<') {ptr--; if (ptr == -1) throw new RangeError('Error: pointer cannot be less than 1')}
+		if      (ch == '+') {if (++mem[ptr] > 127) mem[ptr] = 0}
+		else if (ch == '-') {if (--mem[ptr] < 0) mem[ptr] = 127}
+		else if (ch == '>') ++ptr >= mem.length && mem.push(0)
+		else if (ch == '<') {if (--ptr == -1) throw new RangeError('Error: pointer cannot be less than 1')}
 		else if (ch == '.') res += String.fromCharCode(mem[ptr])
 		else if (ch == ',') mem[ptr] = (config.prompt?.() ?? prompt('enter a ch:')).charCodeAt(0)
 		else if (ch == '[') mem[ptr] == 0 && (ip = find_end(ip))
