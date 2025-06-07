5cbceb9c3110d13899f64df564e67b452e635075
Eli Soli <elisoli+git@vivaldi.net>
2025-06-07 05:30:38 +0000

    moved to codeberg

--- main.js
+++
@@ -1,8 +0,0 @@
-import { bfencoderv2 } from './encoders/encoderv2.js'
-import { bfinterpreter } from './interpreters/complete.js'
-
-const bf = bfencoderv2('Hey! My name is Eli Soli!').next().value
-console.log(bf)
-
-const txt = bfinterpreter(bf.res).next().value
-console.log(txt)
